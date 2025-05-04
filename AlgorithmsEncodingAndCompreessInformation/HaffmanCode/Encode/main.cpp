b937b63c315ea96664db1b794b529783c52ff1c6
mishka8 <144250192+mishka8@users.noreply.github.com>
2025-05-04 15:43:58 +0300

    Update main.cpp

--- AlgorithmsEncodingAndCompreessInformation/HaffmanCode/Encode/main.cpp
+++ AlgorithmsEncodingAndCompreessInformation/HaffmanCode/Encode/main.cpp
@@ -1,71 +1,48 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
+
 
 using namespace std;
-//кодирование
-//Чтение исходного файла
-//Подсчет частот символов
-//
-//Построение дерева Хаффмана
-//
-//Генерация кодов для каждого символа
-//
-//Замена символов на соответствующие коды
-//
-//Сохранение таблицы частот и закодированных данных в файл
-
-
-
-//узел дерева хаффмана
-struct Haffman_block
-{
-    char data;//символ
-    int period;//частота
-    Haffman_block* left;//левый потомок
-    Haffman_block* right;//правый потомок
-
-    Haffman_block(char data, int period) 
-        : data(data), period(period), left(nullptr), right(nullptr) {}
-
-};
-
-struct compare
-{
-    bool operator() (Haffman_block* a, Haffman_block* b)
-    {
-        return a -> period > b -> period;//проверка у кого больше частота
-    }
-};
-
-//читаем файл
-string read_file(const string& filename)
-{
-    ifstream file(filename);
-    if (!file)
-    {
-        throw runtime_error("файл не открыт " + filename);
-    }
-
-    string text;
-
-    char sim;
-    while (file.get(sim))
-    {
-        text += sim;
-    }
-
-    return text;
-}
-
-
-
-
-
 
 
 int main()
 {
+	string filename = "primer.txt";
 
-    cout << "Hello World!\n";
+	ifstream ifs(filename, ifstream::binary);
+
+	if (!ifs)
+	{
+		cerr << "ERROR File is not open";
+	}
+
+	vector<int> freq(256, 0);
+
+
+	while (!ifs.eof())
+	{
+		char ch;
+		ifs.read(&ch, 1);
+		
+		//читаем по одному байту
+
+		freq[static_cast<unsigned char>(ch)]++;
+		//cout << static_cast<unsigned char>(ch) << ", ";
+
+		//обычный char может быть отрицательный
+		//размер вектора не может быть отрицательным
+		//поэтому приводим с помощьлю stasic_cast к usigned char
+
+	}
+
+	/*for (int i = 0; i < 256; ++i)
+	{
+		cout << "[" << i << "] = " << freq[i] << ", ";
+		if (i % 10 == 0 && i != 0)
+			cout << endl;
+	}
+	cout << endl;*/
 
 }
