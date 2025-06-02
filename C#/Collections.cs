a4bfc48ddde7720e3c28274a709d1307f377c94f
mishka8 <144250192+mishka8@users.noreply.github.com>
2025-06-02 16:37:16 +0300

    Update Collections.cs

--- C#/Collections.cs
+++ C#/Collections.cs
@@ -1,1 +1,67 @@
+using System;
+using System.Collections.Generic;
 
+class Program
+{
+    static void Main()
+    {
+        Stack<int> numbers = new Stack<int>();
+
+        //добавляем числа в стек
+        numbers.Push(5);
+        numbers.Push(10);
+        numbers.Push(15);
+
+        Console.WriteLine("что в стеке:");
+        foreach (int num in numbers)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+
+        // Извлекаем два верхних числа
+        int a = numbers.Pop();
+        int b = numbers.Pop();
+        Console.WriteLine("достали - " + a + " " + b);
+        Console.WriteLine();
+
+        // Складываем их
+        int sum = a + b;
+        Console.WriteLine("Сумма - " + sum);
+        Console.WriteLine();
+
+        //обратно
+        numbers.Push(sum);
+
+        Console.WriteLine("что осталось в стеке");
+        foreach (int num in numbers)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+
+        a = numbers.Pop();
+        b = numbers.Pop();
+
+        Console.WriteLine("достали - " + a + " " + b);
+        Console.WriteLine();
+
+        sum = a + b;
+
+        Console.WriteLine("Сумма - " + sum);
+        Console.WriteLine();
+
+
+        numbers.Push(sum);
+        Console.WriteLine("что осталось");
+
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+
+
+    }
+}
